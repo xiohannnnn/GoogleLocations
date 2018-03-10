985f6ee59fabc2e4271ba9c3a8212fbc5d2ac92a
Simon <simjenner3@gmail.com>
2018-03-10 17:11:14 +0000

    Added marker clustering + more logging

--- source/res/main.js
+++ source/res/main.js
@@ -1,40 +1,59 @@
 // Before doing anything, hide loading div
 $("#loading").hide();
 
+// Let user know they should look at the console
+// Probably put this in a closable div at some point
+alert(
+  `
+  /////////// WARNING \\\\\\\\\\\\\\\\\\\\\\
+  Performance may drop if you are
+  loading hundreds of thousands
+  of co-ordinates
+  ----------- READ ME -----------
+  This website outputs a lot of
+  useful information to the
+  console, press F12 and then
+  select the 'Console' tab to see
+  what is going on
+  `
+);
+
 
 // * * * * * * * * * //
-// Fancy file upload stuff
+// File upload + Parsing + Plotting
 // * * * * * * * * * //
-function onChange(event) {
-  clear_markers();
-  $("#loading").show();
-  let reader = new FileReader();
-  reader.onload = onReaderLoad;
-  reader.readAsText(event.target.files[0]);
-}
-
-function onReaderLoad(event){
-  let obj = JSON.parse(event.target.result);
-  loadLocations(obj);
-}
-
-function loadLocations(data){
+function onReaderDone(event){
+  console.log("Read done, parsing");
+  let data = JSON.parse(event.target.result);
   let divideBy = data.locations.length / 100;
+
+  console.log("Processing lat/long from JSON obj");
   $.each(data.locations, function(key, val) {
     let lat = val.latitudeE7 / 10000000;
     let lng = val.longitudeE7 / 10000000;
-    create_marker(lat, lng);
-
+    createMarker(lat, lng);
     if (key % 10000 == 0) {
       console.log(Math.round(key / divideBy) + "% processed");
     }
-
   });
+
+  console.log("100% processed");
   $("#loading").hide();
-  console.log("100% processed")
+  console.log("All processing done");
+
+  let mbUsed = window.performance.memory.usedJSHeapSize / 1000000;
+  console.log("Currently using " + mbUsed + "MB of memory");
+
 }
 
-document.getElementById("upload").addEventListener("change", onChange);
+document.getElementById("upload").addEventListener("change", (event) => {
+  clearMarkers();
+  $("#loading").show();
+  console.log("Reading file");
+  let reader = new FileReader();
+  reader.onload = onReaderDone;
+  reader.readAsText(event.target.files[0]);
+});
 
 
 // * * * * * * * * * //
@@ -43,6 +62,7 @@ document.getElementById("upload").addEventListener("change", onChange);
 let marker_positions = []; // So there aren"t multiple markers in the same place
 
 function initMap() {
+  console.log("Initialising map");
   geocoder = new google.maps.Geocoder();
   map = new google.maps.Map(
     document.getElementById("map"),
@@ -55,6 +75,12 @@ function initMap() {
       mapTypeId: "hybrid"
     }
   );
+  console.log("Initialising markerClusterer");
+  markerClusterer = new MarkerClusterer(
+    map,
+    [],
+    {imagePath:"markerclusterer/images/m"}
+  );
 }
 
 
@@ -62,7 +88,8 @@ function initMap() {
 // Utility Function(s)
 // * * * * * * * * * //
 function search() {
-  let address = document.getElementById("search_box").value;
+  let address = document.getElementById("searchBox").value;
+  console.log("Searching for " + address);
   if (address != "") {
     geocoder.geocode({
         "address": address,
@@ -79,11 +106,13 @@ function search() {
   }
 }
 
-function clear_markers() {
+function clearMarkers() {
+  console.log("Clearing all markers");
   marker_positions = [];
+  markerClusterer.clearMarkers();
 }
 
-function create_marker(lat, lng) {
+function createMarker(lat, lng) {
   let current_lat_lng = lat.toString() + lng.toString();
   if (marker_positions.includes(current_lat_lng)) {
     // Do nothing, dont need multiple markers in one place
@@ -91,24 +120,25 @@ function create_marker(lat, lng) {
     let marker = new google.maps.Marker({
         position: new google.maps.LatLng(lat, lng),
         map: map,
-        icon: "res/dot.png"
+        icon: "res/redDot.png"
     });
+    markerClusterer.addMarker(marker);
     marker_positions.push(current_lat_lng);
   }
 }
 
-function get_my_loc() {
+function getUserLoc() {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(success_callback, error_callback);
+    navigator.geolocation.getCurrentPosition(locSuccessCallback, locErrorCallback);
   }
 }
 
-function success_callback(position) {
+function locSuccessCallback(position) {
   let new_location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
   map.panTo(new_location);
 }
 
-function error_callback(error) {
+function locErrorCallback(error) {
   switch(error.code) {
     case error.PERMISSION_DENIED:
       alert("Denied request for Geolocation");
