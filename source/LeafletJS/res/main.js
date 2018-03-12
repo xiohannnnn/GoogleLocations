2a32e2fe192c2a7af7dfac9ae14140cacc7f1e52
Simon <thatguywiththatname@users.noreply.github.com>
2018-03-12 18:34:38 +0000

    Change default zoom level 1 -> 2

--- source/LeafletJS/res/main.js
+++ source/LeafletJS/res/main.js
@@ -61,7 +61,7 @@ document.getElementById("uploadButton").addEventListener("change", (event) => {
 let marker_positions = [];  // So there aren't multiple markers in the same place
 
 console.log("Initialising map");
-map = L.map("map").setView([0, 0], 1);
+map = L.map("map").setView([0, 0], 2);
 L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
